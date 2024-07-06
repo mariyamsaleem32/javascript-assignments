d890cf8ec3f410b2bcf0df319ca2caab8b761d7c
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2024-07-07 03:23:25 +0500

    add question 2

--- assignment 9 to 11/script.js
+++ assignment 9 to 11/script.js
@@ -1,10 +1,27 @@
 /* 1. Write a program to take “city” name as input from user. If
 user enters “Karachi”, welcome the user like this:
 “Welcome to city of lights” */
-var city = prompt("Enter your city");
-if (city === "karachi") {
-    console.log("welcome to city of light");
-}
-else{
-    console.log("welcome to"+" "+city);
-}
+// var city = prompt("Enter your city");
+// if (city === "karachi") {
+//     console.log("welcome to city of light");
+// }
+// else{
+//     console.log("welcome to"+" "+city);
+// }
+
+/* 2. Write a program to take “gender” as input from user. If the
+user is male, give the message: Good Morning Sir. If the
+user is female, give the message: Good Morning Ma’am. */
+// var gender = prompt("Enter your gender");
+// if (gender === "male") {
+//     console.log("good morning sir");
+// }
+// else if(gender === "female"){
+//     console.log("Good Morning Ma'am");
+// }
+// else{
+//     console.log("good morning"+" "+gender);
+// }
+
+/*3. Write a program to take input color of road traffic signal
+from the user & show the message according to this table: */
