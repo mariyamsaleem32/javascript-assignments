28d5beeafcb9dba0f32660eca206d0e488b28c7e
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2024-07-08 01:51:00 +0500

    add question 1

--- assignment 12 to 13/script.js
+++ assignment 12 to 13/script.js
@@ -6,4 +6,18 @@
 in a variable & checks whether the given input is a
 number, uppercase letter or lower case letter. (Hint: ASCII
 codes:- A=65, Z=90, a=97, z=122).*/
-var 
+var charCode = prompt("Enter a character (number or string)");
+
+    if (charCode >= 48 && charCode <= 57) {
+        console.log(charCode + " is a number");
+    }
+    else if (charCode >= 65 && charCode <= 90) {
+        console.log(charCode + " is an uppercase letter");
+    }
+    else if (charCode >= 97 && charCode <= 122) {
+        console.log(charCode + " is a lowercase letter");
+    }
+    else {
+        console.log(charCode + " is not a number or a letter");
+    }
+
