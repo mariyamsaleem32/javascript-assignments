ee0e860cc55e45c8cf869ba5159df14bfd964fb6
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2024-07-08 01:43:10 +0500

    add assignment 12 to 13

---
+++ assignment 12 to 13/script.js
@@ -0,0 +1,9 @@
+// IF...ELSE & ELSE IF STATEMENT
+// TESTING SET OF CONDITIONS
+
+/*
+1. Write a program that takes a character (number or string)
+in a variable & checks whether the given input is a
+number, uppercase letter or lower case letter. (Hint: ASCII
+codes:- A=65, Z=90, a=97, z=122).*/
+var 
